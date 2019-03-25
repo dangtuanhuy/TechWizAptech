0170b7d0680accf5bd4419c503d508a3d983acb2
Unknown <phongb1505902@student.ctu.edu.vn>
2019-03-26 00:49:20 +0700

    HomeJobs

--- CanThoAptech/WorkGenerator/Areas/Customer/Controllers/HomeController.cs
+++ CanThoAptech/WorkGenerator/Areas/Customer/Controllers/HomeController.cs
@@ -4,6 +4,8 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WorkGenerator.Data;
 using WorkGenerator.Models;
 
 namespace WorkGenerator.Controllers
@@ -11,7 +13,12 @@ namespace WorkGenerator.Controllers
     [Area("Customer")]
     public class HomeController : Controller
     {
-       
+        private readonly ApplicationDbContext _db;
+
+        public HomeController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
         public IActionResult Index()
         {
             return View();
@@ -19,6 +26,11 @@ namespace WorkGenerator.Controllers
         public IActionResult Index2()
         {
             return View();
+        }
+        public IActionResult HomeJobs()
+        {
+            var lst = _db.Job.Include(m=>m.Category).Where(m => m.Status);
+            return View(lst);
         }
         public IActionResult Privacy()
         {
